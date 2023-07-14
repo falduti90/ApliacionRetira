67a4c9ee10ea916b6d45ad7bc5e7f93b04b5323b
Sistemas3 <matiasf@cromosol.com>
2023-07-14 17:04:41 -0300

    Comentario capa entidad

--- CapaNegocio/CN_ControlCabecera.cs
+++ CapaNegocio/CN_ControlCabecera.cs
@@ -8,10 +8,12 @@ using CapaEntidad;
 
 namespace CapaNegocio
 {
+    // Se declara una instancia  de negocio por si hay que aplicar  logica . Actualmente  devuelve la lista que se genero en la instacia
+    // de datos.
+    //Esto Aplica para todas  las clases de la capa entidad
     public class CN_ControlCabecera
     {
         private CD_ControlCabecera ObjCapaDatos = new CD_ControlCabecera();
-
         public List<ControlCabecera> Listar(string Muelle)
         {
             return ObjCapaDatos.Listar(Muelle);
